140806cc45d0804e0642d922ad772565e072e827
mendozaarjay <mendozaarjay.05@gmail.com>
2019-07-01 20:40:05 +0800

    added basic controller

---
+++ SapphireAPI/SapphireAPI/Controllers/CompanyController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SapphireAPI.Models;
+using SapphireAPI.Persistence;
+
+namespace SapphireAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly SapphireDbContext _context;
+
+        public CompanyController(SapphireDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Company
+        [HttpGet]
+        public IEnumerable<Company> GetCompany()
+        {
+            return _context.Company;
+        }
+
+        // GET: api/Company/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCompany([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var company = await _context.Company.FindAsync(id);
+
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(company);
+        }
+
+        // PUT: api/Company/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCompany([FromRoute] int id, [FromBody] Company company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != company.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(company).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompanyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Company
+        [HttpPost]
+        public async Task<IActionResult> PostCompany([FromBody] Company company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Company.Add(company);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCompany", new { id = company.Id }, company);
+        }
+
+        // DELETE: api/Company/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCompany([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var company = await _context.Company.FindAsync(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            _context.Company.Remove(company);
+            await _context.SaveChangesAsync();
+
+            return Ok(company);
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return _context.Company.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ SapphireAPI/SapphireAPI/Persistence/SapphireDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SapphireAPI.Models;
+
+namespace SapphireAPI.Persistence
+{
+    public class SapphireDbContext : DbContext
+    {
+        public SapphireDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<SapphireAPI.Models.Company> Company { get; set; }
+    }
+}
